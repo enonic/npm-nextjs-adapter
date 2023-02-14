3823cbfc46a2956fd11c6b1f3a1d6ac622d67d51
Pavel Milkevich <pmi@enonic.com>
2023-02-14 10:59:13 +0100

    Move exported members to root of nextjs-adapter #88

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 export * from './utils';
 export * from './guillotine/fetchContent';
+export * from './guillotine/getMetaData';
+export * from './ComponentRegistry';
+export * from './UrlProcessor';
