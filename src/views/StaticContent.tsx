d5aaef6215de94c5988fa37f80aac9fb050ba695
Persijn Kwekkeboom <pkw@enonic.com>
2023-06-29 14:24:07 +0200

    Dispache bug fix #235

--- src/views/StaticContent.tsx
+++ src/views/StaticContent.tsx
@@ -2,21 +2,14 @@ import {createElement, createRef, ReactNode, useEffect, useState} from 'react';
 
 function useStaticContent(ref, isServer) {
     if (isServer) {
-        return true;
+        return false;
     }
 
-    const [render, setRender] = useState(isServer);
+    if (ref?.current?.innerHTML === '') {
+        return false;
+    }
 
-    useEffect(() => {
-        // check if the innerHTML is empty as client side navigation
-        // need to render the component without server-side backup
-        const isEmpty = ref?.current?.innerHTML === '';
-        if (isEmpty) {
-            setRender(true);
-        }
-    }, []);
-
-    return render;
+    return true;
 }
 
 export interface StaticContentProps extends Record<string, any> {
@@ -28,20 +21,20 @@ export interface StaticContentProps extends Record<string, any> {
 export default function StaticContent({children, element = 'div', condition = true, ...props}: StaticContentProps) {
     const isServer = typeof window === 'undefined';
     const ref = createRef();
-    const render = useStaticContent(ref, isServer);
+    const isStatic = useStaticContent(ref, isServer);
+
+    // avoid re-render on the client
+    if (isStatic && condition) {
+        return createElement(element, {
+            ...props,
+            ref,
+            suppressHydrationWarning: true,
+            dangerouslySetInnerHTML: {__html: ''},
+        });
+    }
 
     // just render if:
     // - condition is false
     // - we're in the server or a spa navigation
-    if (render || !condition) {
-        return createElement(element, props, ...[].concat(children));
-    }
-
-    // avoid re-render on the client
-    return createElement(element, {
-        ...props,
-        ref,
-        suppressHydrationWarning: true,
-        dangerouslySetInnerHTML: {__html: ''},
-    });
+    return createElement(element, props, ...[].concat(children));
 }
