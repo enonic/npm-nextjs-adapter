a07288d7ced913219856b90b2a615cb1c8b5f1d9
Pavel Milkevich <pmi@enonic.com>
2024-01-08 17:19:26 +0100

    Create I18n methods for server-side translations #434 Create LocaleContext for client-side translations #435

---
+++ src/i18n/i18n.ts
@@ -0,0 +1,55 @@
+export type Dict = {
+    [key: string]: string,
+};
+
+export class I18n {
+    private static locale = '';
+    private static dictionary: Dict = {};
+
+    public static async setLocale(locale: string): Promise<Dict> {
+        this.locale = locale;
+        return loadPhrases(locale).then((phrases) => {
+            this.dictionary = phrases;
+            console.info(`Loaded server-side phrases for locale "${locale}"`);
+            return phrases;
+        });
+    }
+
+    public static localize(key: string, ...args: any[]): string {
+        return getPhrase(this.locale, this.dictionary, key, ...args);
+    }
+
+    public static getLocale() {
+        return this.locale;
+    }
+
+    public static getDictionary() {
+        return this.dictionary;
+    }
+}
+
+export const loadPhrases = async (locale: string): Promise<Dict> => {
+    return import(`@phrases/${locale}.json`)
+        .then((module) => module.default)
+        .catch((e) => {
+            console.error(`Failed to load phrases for locale "${locale}"`, e);
+            return {};
+        });
+};
+
+export const getPhrase = (locale: string, dict: Dict, key: string, ...args: any[]) => {
+    const template = dict[key];
+    if (!template) {
+        console.warn(`Missing localization phrase for locale "${locale}": <${key}>`);
+        return `<${key}>`;
+    }
+    if (args?.length) {
+        let index = -1;
+        return template.replace(/{(\d+)?}/g, (match, digit) => {
+            index++;
+            return args[digit || index] ?? match;
+        });
+    }
+    return template;
+};
+
