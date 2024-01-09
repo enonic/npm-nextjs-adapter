38aa4b0f8fa672074ea78504472d68d7295658e6
Christian Westgaard <comlock@gmail.com>
2024-01-09 12:35:31 +0100

    addLocaleHeaders and setJsessionCookieHeader -> buildGuillotineRequestHeaders

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,8 @@
 // Seems like NodeJS.fetch lowercases all headers, so we need to lowercase the
 // header names here.
-export enum REQUEST_HEADERS {
+export enum GUILLOTINE_REQUEST_HEADERS {
     COOKIE = 'Cookie',
+    LOCALE = 'locale',
+    LOCALES = 'locales',
+    DEFAULT_LOCALE = 'default-locale',
 }
