42552000d4dc38e0e57562ac222af3d645d60fef
Christian Westgaard <comlock@gmail.com>
2024-01-16 15:34:16 +0100

    Fix linting errors

--- src/guillotine/fetchContentPathsForAllLocales.ts
+++ src/guillotine/fetchContentPathsForAllLocales.ts
@@ -8,10 +8,12 @@ import {fetchContentPathsForLocale} from './fetchContentPathsForLocale';
 export async function fetchContentPathsForAllLocales(
     path: string,
     query: string = GET_STATIC_PATHS_QUERY,
-    countPerLocale = 999
+    countPerLocale = 999,
 ): Promise<ContentPathItem[]> {
     const promises = Object.values(getProjectLocaleConfigs()).map(
-        config => fetchContentPathsForLocale(path, config, query, countPerLocale)
+        config => fetchContentPathsForLocale(
+            path, config, query, countPerLocale,
+        ),
     );
     return Promise.all(promises).then(results => {
         return results.reduce((all, localePaths) => all.concat(localePaths), []);
