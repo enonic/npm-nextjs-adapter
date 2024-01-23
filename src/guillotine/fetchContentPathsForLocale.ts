457fb6d8b8639089526ba6c46e78dc85395db29c
Pavel Milkevich <pmi@enonic.com>
2024-01-23 10:44:21 +0100

    Paths for static generation have extra empty slug in the beginning #456
    - used string literal
    - added tests

--- src/guillotine/fetchContentPathsForLocale.ts
+++ src/guillotine/fetchContentPathsForLocale.ts
@@ -30,7 +30,7 @@ export async function fetchContentPathsForLocale(
             prev: ContentPathItem[],
             child: any,
         ) => {
-            const regexp = new RegExp(`^/${child.site ? child.site._name + '/?' : ''}`);
+            const regexp = new RegExp(`^/${child.site?._name ? `${child.site._name}/?` : ''}`);
             const contentPath = child._path.replace(regexp, '');
             prev.push({
                 contentPath: contentPath.split('/'),
